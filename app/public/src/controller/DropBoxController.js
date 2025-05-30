093402ab1aab324a779a99d62a129c2c82d8ddcc
rafokez <raphael.k.business@gmail.com>
2025-05-30 12:27:35 -0300

    feat: fixing routes

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -22,15 +22,49 @@ class DropBoxController {
 
             this.uploadTask(event.target.files);
 
-            this.snackModalEl.style.display = 'block'; 
+            this.snackModalEl.style.display = 'block';
 
         })
-        
+
     }
 
-    uploadTask(files){
+    uploadTask(files) {
 
         let promises = [];
+
+        [...files].forEach(file => {
+
+            promises.push(new Promise((resolve, reject) => {
+
+                let ajax = new XMLHttpRequest();
+
+                ajax.open('POST', '/upload');
+
+                ajax.onload = event => {
+                    try {
+
+                        resolve(JSON.parse(ajax.responseText));
+
+                    } catch (e) {
+
+                        reject(e);
+
+                    }
+                }
+
+                ajax.onerror = event => {
+                    reject(event);
+                }
+
+                let formData = new FormData();
+
+                formData.append('input-file', file);
+
+                ajax.send(formData);
+
+            }));
+
+        });
 
         return Promise.all(promises);
 
