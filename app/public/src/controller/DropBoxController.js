48819f8815b680b28df3f119745105047bb69e4b
rafokez <raphael.k.business@gmail.com>
2025-06-02 14:26:04 -0300

    feat: rename button functionality

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -42,6 +42,24 @@ class DropBoxController {
 
     initEvents() {
 
+        this.btnRename.addEventListener('click', e=>{
+        
+            let li = this.getSelection()[0];
+
+            let file = JSON.parse(li.dataset.file);
+
+            let name = prompt("Renomear o arquivo", file.name);
+
+            if (name) {
+
+                file.name = name; 
+
+                this.getFirebaseRef().child(li.dataset.key).set(file);
+
+            }
+
+        })
+
         this.listFilesEl.addEventListener('selectionchange', e => {
             switch (this.getSelection().length) {
 
@@ -362,6 +380,7 @@ class DropBoxController {
         let li = document.createElement('li');
 
         li.dataset.key = key;
+        li.dataset.file = JSON.stringify(file);
 
         li.innerHTML = `
             <li>
