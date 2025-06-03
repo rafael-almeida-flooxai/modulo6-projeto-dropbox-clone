ceff4bf80543f0ff7dd33b8543f011e00026cebb
rafokez <raphael.k.business@gmail.com>
2025-06-03 12:53:50 -0300

    feat: fixing code and adding span directory

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -5,6 +5,7 @@ class DropBoxController {
 
         this.onselectionchange = new Event('selectionchange');
 
+        this.navEl = document.querySelector('#browse-location');
         this.btnSendFileEl = document.querySelector('#btn-send-file');
         this.inputFilesEl = document.querySelector('#files');
         this.snackModalEl = document.querySelector('#react-snackbar-root');
@@ -20,6 +21,7 @@ class DropBoxController {
 
         this.connectFirebase();
         this.initEvents();
+        this.openFolder();
         this.readFiles();
     }
 
@@ -66,14 +68,14 @@ class DropBoxController {
 
     initEvents() {
 
-        this.btnNewFolder.addEventListener('click', e =>{
+        this.btnNewFolder.addEventListener('click', e => {
             let name = prompt('Nome da nova pasta:');
 
             if (name) {
                 this.getFirebaseRef().push().set({
                     name,
                     type: 'folder',
-                    path:this.currentFolder.join('/')
+                    path: this.currentFolder.join('/')
                 })
             }
         })
@@ -165,8 +167,11 @@ class DropBoxController {
         this.btnSendFileEl.disabled = false;
     }
 
-    getFirebaseRef() {
-        return firebase.database().ref('files');
+    getFirebaseRef(path) {
+
+        if (!path) path = this.currentFolder.join('/');
+
+        return firebase.database().ref(path);
     }
 
     modalShow(show = true) {
@@ -177,40 +182,27 @@ class DropBoxController {
 
     ajax(url, method = 'GET', formData = new FormData(), onprogress = function () { }, onloadstart = function () { }) {
 
-        return new Promise((resolve, reject => {
-
+        return new Promise((resolve, reject) => {
             let ajax = new XMLHttpRequest();
 
-            ajax.open('method', 'url');
+            ajax.open(method, url); // Aqui também está errado: você escreveu 'method' e 'url' como string
 
             ajax.onload = event => {
-
                 try {
-
                     resolve(JSON.parse(ajax.responseText));
-
                 } catch (e) {
-
                     reject(e);
-
                 }
-            }
+            };
 
-            ajax.onerror = event => {
-                reject(event);
-            }
-
+            ajax.onerror = event => reject(event);
             ajax.upload.onprogress = onprogress;
 
             onloadstart();
-
             ajax.send(formData);
-
-        }))
+        });
 
     }
-
-
 
     uploadTask(files) {
 
@@ -222,7 +214,7 @@ class DropBoxController {
 
             formData.append('input-file', file);
 
-            promises.push(this.ajax('/upload', 'POST', formData, () => {
+            promises.push(this.ajax(window.location.origin + '/upload', 'POST', formData, () => {
                 this.uploadProgress(event, file);
 
             }, () => {
@@ -461,6 +453,8 @@ class DropBoxController {
 
     readFiles() {
 
+        this.lastFolder = this.currentFolder.join('/');
+
         this.getFirebaseRef().on('value', snapshot => {
 
             this.listFilesEl.innerHTML = '';
@@ -470,7 +464,11 @@ class DropBoxController {
                 let key = snapshotItem.key;
                 let data = snapshotItem.val();
 
-                this.listFilesEl.appendChild(this.getFileView(data, key));
+                if (data.type) {
+                    this.listFilesEl.appendChild(this.getFileView(data, key));
+
+                }
+
 
             });
 
@@ -478,7 +476,85 @@ class DropBoxController {
 
     }
 
+    openFolder() {
+
+        if (this.lastFolder) this.getFirebaseRef(this.lastFolder).off('value');
+
+        this.renderNav();
+        this.readFiles();
+
+    }
+
+    renderNav() {
+
+        let nav = document.createElement('nav');
+        let path = [];
+
+        for (let i = 0; i < this.currentFolder.length; i++) {
+            let folderName = this.currentFolder[i];
+            let span = document.createElement('span');
+
+            path.push(folderName);
+
+            if (i + 1 === this.currentFolder.length) {
+
+                span.innerHTML = folderName;
+
+                nav.appendChild(span);
+
+            } else {
+
+                span.className = 'breadcrumb-segment_wrapper';
+                span.innerHTML = `<span class="ue-effect-container uee-BreadCrumbSegment-link-0">
+                    <a href="#" data-path="${path.join('/')}" class="breadcrumb-segment">${folderName}</a>
+                            </span>
+                                    <svg width="24" height="24" viewBox="0 0 24 24" class="mc-icon-template-stateless" style="top: 4px; position: relative;">
+                                    <title>arrow-right</title>
+                            <path d="M10.414 7.05l4.95 4.95-4.95 4.95L9 15.534 12.536 12 9 8.464z" fill="#637282" fill-rule="evenodd"></path>
+                    </svg>`
+            }
+
+            nav.appendChild(span);
+        }
+
+        this.navEl.innerHTML = nav.innerHTML;
+
+        this.navEl.querySelectorAll('a').forEach(a=>{
+            a.addEventListener('click', e=>{
+                e.preventDefault();
+
+                this.currentFolder = a.dataset.path.split('/');
+                
+            })
+        })
+
+        /* 
+        <span class="breadcrumb-segment__wrapper">
+        
+        </span>
+        */
+
+    }
+
     initEventsLi(li) {
+
+        li.addEventListener('dblclick', e => {
+
+            let file = JSON.parse(li.dataset.file);
+
+            switch (file.type) {
+                case 'folder':
+                    this.currentFolder.push(file.name);
+                    this.openFolder();
+
+                    break
+
+                default:
+                    window.open('/file?path=' + file.path);
+            }
+
+        })
+
         li.addEventListener('click', e => {
 
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,18 @@
-var express = require('express');
-var router = express.Router();
-var formidable = require('formidable');
-var fs = require('fs');
+const express = require('express');
+const router = express.Router();
+const formidable = require('formidable');
+const fs = require('fs');
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.delete('file', (req, res) => {
+router.delete('/file', (req, res) => {
   let form = new formidable.IncomingForm({
     uploadDir: './upload',
-    keepExtesions: true
+    keepExtensions: true
   })
 
   form.parse(req, (err, fields, files) => {
@@ -25,7 +26,7 @@ router.delete('file', (req, res) => {
         if (err) {
 
           res.status(400).json({
-            
+
             err
 
           });
@@ -46,24 +47,51 @@ router.delete('file', (req, res) => {
 })
 
 router.post('/upload', (req, res) => {
-
-  let form = new formidable.IncomingForm({
-    uploadDir: './upload',
-    keepExtesions: true
-  })
-
-  form.parse(req, (err, fields, files) => {
-
-    res.json({
-
-      files
-
-    });
-
+  const form = new formidable.IncomingForm({
+    uploadDir: path.join(__dirname, '../upload'),
+    keepExtensions: true
   });
 
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      console.error('Erro ao processar upload:', err);
+      return res.status(500).json({ error: 'Erro no upload' });
+    }
 
+    const uploadedFile = files['input-file'];
 
-})
+    if (!uploadedFile) {
+      return res.status(400).json({ error: 'Nenhum arquivo recebido.' });
+    }
+
+    // Formidable pode retornar array ou objeto direto
+    const file = Array.isArray(uploadedFile) ? uploadedFile[0] : uploadedFile;
+
+    const fileData = {
+      name: file.originalFilename || file.name,
+      type: file.mimetype,
+      path: path.basename(file.filepath || file.path)
+    };
+
+    res.json({ files: { 'input-file': fileData } });
+  });
+});
+
+router.get('/file', (req, res) => {
+  const fileName = req.query.path;
+
+  if (!fileName) {
+    return res.status(400).send('Parâmetro "path" é obrigatório');
+  }
+
+  const filePath = path.join(__dirname, '../upload', fileName);
+
+  res.sendFile(filePath, err => {
+    if (err) {
+      console.error('Erro ao enviar arquivo:', err);
+      res.status(404).send('Arquivo não encontrado');
+    }
+  });
+});
 
 module.exports = router;
